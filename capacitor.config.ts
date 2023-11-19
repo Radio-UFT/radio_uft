4f1685d76d66975596965f3ba931bfde7c5fdf00
armanoalves <armanobarrosalvesh@gmail.com>
2023-11-19 17:55:13 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'radio_uft',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
